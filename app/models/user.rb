2c01afa9905df961ac3f74d0b1c1ea4372cf7930
Ricardo Ruy <ricardo.rruy@hotmail.com>
2024-02-21 08:51:51 -0300

    Create User Controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,15 @@
 class User < ApplicationRecord
   has_many :guesses
+
+  validate :validate_winner_status, on: :update
+
+  private
+
+  def validate_winner_status
+    if winner_changed? &&
+       winner? &&
+       winner_previously_changed?
+      errors.add(:winner, 'User can only become a winner once')
+    end
+  end
 end
