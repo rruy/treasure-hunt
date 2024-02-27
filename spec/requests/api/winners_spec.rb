b236e18255bec0b8130c2e7c8f9684761adb5e3b
Ricardo Ruy <ricardo.rruy@hotmail.com>
2024-02-27 00:03:47 -0300

    Added parameter page for winners into swagger api doc

--- spec/requests/api/winners_spec.rb
+++ spec/requests/api/winners_spec.rb
@@ -4,18 +4,21 @@ RSpec.describe 'Api::WinnersController', type: :request do
   let!(:user) { create(:user) }
   let!(:auth_token) { user.create_new_auth_token }
 
-  path '/api/winners?page=1' do
+  path '/api/winners' do
     get('List winners') do
       tags 'Winners'
       produces 'application/json'
+      parameter name: :page, in: :query, type: :integer
 
       response(200, 'successful') do
         let(:Authorization) { "#{auth_token['Authorization']}" }
+        let(:page) { 1 }
         run_test!
       end
 
       response(401, 'unauthorized') do
         let(:Authorization) { nil } # Unauthorized request
+        let(:page) { 1 }
         run_test! do
           expect(response).to have_http_status(:unauthorized)
         end
